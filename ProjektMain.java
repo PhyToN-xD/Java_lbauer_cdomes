146be5c0f44589f3d785c9c97684a7387d9a6fd0
PhyToN-xD <luki.bauer2005@gmail.com>
2022-11-30 08:43:55 +0100

    .

--- ProjektMain.java
+++ ProjektMain.java
@@ -1,5 +1,9 @@
 public class ProjektMain {
   public static void main(String[] args) {
+    lbauer obj1 = new lbauer();
+    cdomes obj2 = new cdomes();
 
+    obj1.tell();
+    obj2.tell();
   }
 }
---
+++ out/production/Java_lbauer_cdomes/README.md
@@ -0,0 +1,2 @@
+# Java_lbauer_cdomes
+Lukas Bauer
